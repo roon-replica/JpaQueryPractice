19c065dbabf82a06e33d1ccdbcc8468d99f6df6c
leemr <think-roon@naver.com>
2022-08-08 22:49:20 +0900

    Collection Value type 사용법..

--- src/main/java/com/example/jpa/entity/Member.java
+++ src/main/java/com/example/jpa/entity/Member.java
@@ -4,7 +4,9 @@ import lombok.Getter;
 
 import javax.persistence.*;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Getter
 @Entity
@@ -17,6 +19,19 @@ public class Member {
     private String username;
 
     private Address address;
+
+    @ElementCollection
+    @CollectionTable(
+            name="favorite_foods",
+            joinColumns = @JoinColumn(name="member_id"))
+    @Column(name="food_name")
+    private Set<String> favoriteFoods = new HashSet<>();
+
+    @ElementCollection
+    @CollectionTable(
+            name="address",
+            joinColumns = @JoinColumn(name="member_id"))
+    private List<Address> addressHistory = new ArrayList<>();
 
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
